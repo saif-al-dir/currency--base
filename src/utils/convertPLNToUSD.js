9ce78e8e0993b6d26382dd919b0050f0d2e9ca94
Saif Al-Diresee <saif.aldiresee@gmail.com>
2025-05-03 15:53:13 +0200

    Test typeof

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,7 +1,10 @@
 export const convertPLNToUSD = (PLN) => {
 
+  if (typeof PLN === 'string') return NaN;
+  if (typeof PLN !== 'number') return 'Error';
+  if (PLN < 0) return '$0.00';
   const PLNtoUSD = PLN / 3.5;
-  
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
